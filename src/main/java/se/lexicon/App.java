a4493f102b77e1ef194a33699608b8064edff57c
binleiden <daniel.petersson83@gmail.com>
2024-02-27 09:23:47 +0100

    Initial commit

---
+++ src/main/java/se/lexicon/App.java
@@ -0,0 +1,22 @@
+package se.lexicon;
+
+import org.w3c.dom.ls.LSOutput;
+
+/**
+ * Hello world!
+ */
+public class App {
+//single line comment.
+    /*
+    This is  a mulit-line comment
+    /*
+
+
+    public static void main( String[] args )
+    {
+        //sout + tab
+        System.out.println( "Hello World!" );
+
+    }
+
+}
